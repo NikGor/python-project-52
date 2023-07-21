6d364e730940235ed6475e578c3e3e8e6f53f130
Nikolai <nicolas.gordienko@gmail.com>
2023-07-21 16:48:23 +0200

    fix: fix task filter

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.views import View
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic.edit import FormView, DeleteView
 from django.urls import reverse_lazy
 from .forms import TaskForm, FilterForm
@@ -9,23 +8,22 @@ from users.models import User
 from labels.models import Label
 from django.contrib import messages
 from django.utils.translation import gettext as _
+from django.views.generic import DetailView, ListView
 
 
-class TaskDetailView(View):
-    def get(self, request, pk):
-        task = get_object_or_404(Task, pk=pk)
-        labels = task.labels.all()
-        return render(request, 'tasks/task_detail.html', {'task': task, 'labels': labels})
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'tasks/task_detail.html'
 
 
-class TaskListView(View):
-    def get(self, request):
-        tasks = Task.objects.all()
-        statuses = Status.objects.all()
-        users = User.objects.all()
-        labels = Label.objects.all()
+class TaskListView(ListView):
+    model = Task
+    template_name = 'tasks/tasks.html'
+    context_object_name = 'tasks'
 
-        form = FilterForm(request.GET)
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        form = FilterForm(self.request.GET)
 
         if form.is_valid():
             name_search = form.cleaned_data.get('name')
@@ -35,21 +33,25 @@ class TaskListView(View):
             only_mine = form.cleaned_data.get('only_mine')
 
             if only_mine:
-                tasks = tasks.filter(author=request.user)
-            if status_filter:
-                tasks = tasks.filter(status=status_filter)
-            if assignee_filter:
-                tasks = tasks.filter(assignee=assignee_filter)
-            if name_search:
-                tasks = tasks.filter(name__icontains=name_search)
-            if label_filter:
-                tasks = tasks.filter(labels__in=label_filter)
+                queryset = queryset.filter(author=self.request.user)
+            if status_filter is not None:
+                queryset = queryset.filter(status=status_filter)
+            if assignee_filter is not None:
+                queryset = queryset.filter(assignee=assignee_filter)
+            if name_search is not None:
+                queryset = queryset.filter(name__icontains=name_search)
+            if label_filter is not None:
+                queryset = queryset.filter(labels__in=label_filter)
 
-        return render(request, 'tasks/tasks.html', {'form': form,
-                                                    'tasks': tasks,
-                                                    'statuses': statuses,
-                                                    'users': users,
-                                                    'labels': labels})
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = FilterForm(self.request.GET)
+        context['statuses'] = Status.objects.all()
+        context['users'] = User.objects.all()
+        context['labels'] = Label.objects.all()
+        return context
 
 
 class TaskCreateView(FormView):
