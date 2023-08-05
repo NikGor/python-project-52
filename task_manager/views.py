625c7f3354a730d6c7e46f8f08212e9956004d6f
Nikolai <nicolas.gordienko@gmail.com>
2023-08-05 15:21:22 +0200

    review: refactor login/logout view

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,52 +1,35 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView as AuthLoginView, LogoutView as AuthLogoutView
 from django.contrib import messages
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView
 from django.utils.translation import gettext as _
-from django.views import View
 
 
-class IndexView(View):
-    def get(self, request):
-        return render(request, 'task_manager/index.html')
+class IndexView(TemplateView):
+    template_name = 'task_manager/index.html'
 
 
-class LoginView(View):
-    def get(self, request):
-        return render(request=request, template_name="task_manager/login.html",
-                      context={"form": AuthenticationForm()})
+class LoginView(AuthLoginView):
+    template_name = 'task_manager/login.html'
+    redirect_authenticated_user = True
 
-    def post(self, request):
-        form = AuthenticationForm(request, data=request.POST)
-        if not form.is_valid():
-            for field in form:
-                for error in field.errors:
-                    messages.error(request, f"{field.label}: {error}")
-            return render(request=request, template_name="task_manager/login.html",
-                          context={"form": form})
+    def form_valid(self, form):
+        messages.info(self.request, _("Вы залогинены"))
+        return super().form_valid(form)
 
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password')
-        user = authenticate(username=username, password=password)
-        if user is None:
-            messages.error(request, _("Неверное имя пользователя или пароль."))
-            return render(request=request, template_name="task_manager/login.html",
-                          context={"form": form})
+    def form_invalid(self, form):
+        for field in form:
+            for error in field.errors:
+                messages.error(self.request, f"{field.label}: {error}")
+        return super().form_invalid(form)
 
-        login(request, user)
-        messages.info(request, _("Вы залогинены"))
-        return redirect("/")
+    def get_success_url(self):
+        return reverse_lazy('index')
 
 
-class LogoutView(View):
-    def get(self, request):
-        return self.logout(request)
+class LogoutView(AuthLogoutView):
+    next_page = 'index'
 
-    def post(self, request):
-        return self.logout(request)
-
-    @staticmethod
-    def logout(request):
-        logout(request)
+    def dispatch(self, request, *args, **kwargs):
         messages.success(request, _("Вы разлогинены"))
-        return redirect('index')
+        return super().dispatch(request, *args, **kwargs)
